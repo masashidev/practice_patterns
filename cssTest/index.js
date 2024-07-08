297200e09132fbd989e6d1186cb09bb7f6e16525
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-07-08 20:40:32 +0900

    flex

--- cssTest/index.js
+++ cssTest/index.js
@@ -1,9 +1,16 @@
 const app = document.getElementById('app');
 
-for (let i = 0; i < 26; i++) {
+const inputs = [];
+
+for (let i = 0; i < 25; i++) {
   const input = document.createElement('input');
   input.id = 'input';
   input.type = 'text';
   input.placeholder = 'Enter a string';
   app.appendChild(input);
+
+  inputs.push(input);
 }
+
+inputs.forEach((input) => {
+  input.addEventListener('enter', function() {
